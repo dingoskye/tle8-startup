bf9c262f50687d658d9d37fe0469ef927a9d39a4
Skye Jacobs <skyejacobs@casema.nl>
2026-06-02 15:30:08 +0200

    removed unnecessary comments

--- front-end/src/pages/subtask.jsx
+++ front-end/src/pages/subtask.jsx
@@ -119,39 +119,6 @@ function Subtask() {
                         />
                     </div>
 
-                    {/*<div className="relative bg-(--christa-yellow) p-4 shadow-md">*/}
-                    {/*    <div className="absolute -top-2 -left-2 w-8 h-2 bg-amber-700/60 -rotate-45"></div>*/}
-                    {/*    <div className="absolute -top-2 -right-2 w-8 h-2 bg-amber-700/60 rotate-45"></div>*/}
-
-                    {/*    <label htmlFor="niveau" className="block font-bold mb-4">*/}
-                    {/*        Niveau:*/}
-                    {/*    </label>*/}
-
-                    {/*    <div className="relative">*/}
-                    {/*        <input type="range" min="1" max="4" step="1"*/}
-                    {/*            id="niveau"*/}
-                    {/*            name="niveau"*/}
-                    {/*            value={formData.niveau}*/}
-                    {/*            onChange={handleInputChange}*/}
-                    {/*            className="relative z-10 w-full accent-red-600 cursor-pointer"*/}
-                    {/*        />*/}
-
-                    {/*        <div className="pointer-events-none absolute top-1/2 left-0 right-0 flex justify-between px-2 -translate-y-1/2">*/}
-                    {/*            <div className="w-0.5 h-6 bg-black/60"></div>*/}
-                    {/*            <div className="w-0.5 h-6 bg-black/60"></div>*/}
-                    {/*            <div className="w-0.5 h-6 bg-black/60"></div>*/}
-                    {/*            <div className="w-0.5 h-6 bg-black/60"></div>*/}
-                    {/*        </div>*/}
-                    {/*    </div>*/}
-
-
-                    {/*    <div className="flex justify-between text-[10px] mt-2">*/}
-                    {/*        <span>Beginner</span>*/}
-                    {/*        <span>In ontwikkeling</span>*/}
-                    {/*        <span>Gevorderd</span>*/}
-                    {/*        <span>Expert</span>*/}
-                    {/*    </div>*/}
-                    {/*</div>*/}
                     <div className="relative bg-(--christa-yellow) p-4 shadow-md">
 
                         <div className="absolute -top-2 -left-2 w-8 h-2 bg-amber-700/60 -rotate-45"></div>
@@ -161,10 +128,10 @@ function Subtask() {
                             Niveau:
                         </label>
                         <div className="relative">
-                            {/* horizontale lijn */}
+                            {/* lijn */}
                             <div className="absolute left-0 right-0 top-6 h-0.5 bg-black"></div>
 
-                            {/* slider punten */}
+                            {/* slider */}
                             <div className="grid grid-cols-4">
                                 {[1, 2, 3, 4].map((value) => (
                                     <button
@@ -181,15 +148,13 @@ function Subtask() {
                                         {/* verticale lijn */}
                                         <span className="absolute top-0 h-12 w-0.5 bg-black"></span>
 
-                                        {/* rode indicator */}
+                                        {/* slider dot */}
                                         {Number(formData.niveau) === value && (
                                             <span className="absolute top-4 z-30 h-5 w-5 rounded-full border-2 border-red-800 bg-red-600 shadow-md"></span>
                                         )}
                                     </button>
                                 ))}
                             </div>
-
-                            {/* labels */}
                             <div className="grid grid-cols-4 mt-3 text-[10px]">
                                 <span className="text-center">Beginner</span>
                                 <span className="text-center">In ontwikkeling</span>
@@ -197,40 +162,6 @@ function Subtask() {
                                 <span className="text-center">Expert</span>
                             </div>
                         </div>
-
-
-                        {/*<div className="relative flex items-center justify-between">*/}
-
-                        {/*    /!* lijn *!/*/}
-                        {/*    <div className="absolute left-0 right-0 top-1/2 h-0.5 -translate-y-1/2 bg-black"></div>*/}
-
-
-                        {/*    {[1, 2, 3, 4].map((value) => (*/}
-                        {/*        <button*/}
-                        {/*            key={value}*/}
-                        {/*            type="button"*/}
-                        {/*            onClick={() =>*/}
-                        {/*                setFormData((prev) => ({*/}
-                        {/*                    ...prev,*/}
-                        {/*                    niveau: value,*/}
-                        {/*                }))*/}
-                        {/*            }*/}
-                        {/*            className="relative z-20 flex h-12 w-12 items-center justify-center">*/}
-                        {/*            <span className="absolute h-8 w-0.5 bg-black"></span>*/}
-
-                        {/*            {Number(formData.niveau) === value && (*/}
-                        {/*                <span className="relative z-30 h-5 w-5 rounded-full border-2 border-red-800 bg-red-600 shadow-md"></span>*/}
-                        {/*            )}*/}
-                        {/*        </button>*/}
-                        {/*    ))}*/}
-                        {/*</div>*/}
-                        {/*<div className="flex justify-between text-[10px] mt-2">*/}
-                        {/*    <span>Beginner</span>*/}
-                        {/*    <span>In ontwikkeling</span>*/}
-                        {/*    <span>Gevorderd</span>*/}
-                        {/*    <span>Expert</span>*/}
-                        {/*</div>*/}
-
 
                     </div>
                     <button type="submit" className="self-center mt-16 px-8 py-2 rounded-full bg-[#ddaefe] text-black border-2 border-white shadow-md font-bold hover:scale-105 transition">
