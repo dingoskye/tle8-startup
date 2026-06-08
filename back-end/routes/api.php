1d2803d02076cbb774ca440044e45a9d6252d8bd
Thomas van Bockel <Tnvanbockel@gmail.com>
2026-06-08 10:58:24 +0200

    added jwt middleware, and added the middle ware to the api

--- back-end/routes/api.php
+++ back-end/routes/api.php
@@ -8,32 +8,33 @@ use App\Http\Controllers\GroupController;
 use Illuminate\Support\Facades\Route;
 
 // User controller routes
-
-Route::get('/user/{id}', [UserController::class, 'show']);
-Route::get('/user', [UserController::class, 'index']);
-Route::put('/user/edit/{id}', [UserController::class, 'edit']);
-
-
 Route::post('/user/login', [UserController::class, 'login']);
 Route::post('/user/register', [UserController::class, 'register']);
 
+Route::middleware('jwt')->group(function () {
+    Route::get('/user/{id}', [UserController::class, 'show']);
+    Route::get('/user', [UserController::class, 'index']);
+    Route::put('/user/edit/{id}', [UserController::class, 'edit']);
+
+
 //Group controller routes
-Route::post('/group/create', [GroupController::class, 'create']);
-Route::get('/group/', [GroupController::class, 'index']);
-Route::get('/group/{id}', [GroupController::class, 'show']);
-Route::put('/group/edit/{id}', [GroupController::class, 'edit']);
-Route::delete('/group/delete/{id}', [GroupController::class, 'delete']);
+    Route::post('/group/create', [GroupController::class, 'create']);
+    Route::get('/group/', [GroupController::class, 'index']);
+    Route::get('/group/{id}', [GroupController::class, 'show']);
+    Route::put('/group/edit/{id}', [GroupController::class, 'edit']);
+    Route::delete('/group/delete/{id}', [GroupController::class, 'delete']);
 
 //Main task controller routes
-Route::post('/main/create', [MainTaskController::class, 'create']);
-Route::get('/main/{id}', [MainTaskController::class, 'index']);
-Route::get('/main/details/{id}', [MainTaskController::class, 'show']);
-Route::put('/main/edit/{id}', [MainTaskController::class, 'edit']);
-Route::delete('/main/delete/{id}', [MainTaskController::class, 'delete']);
+    Route::post('/main/create', [MainTaskController::class, 'create']);
+    Route::get('/main/{id}', [MainTaskController::class, 'index']);
+    Route::get('/main/details/{id}', [MainTaskController::class, 'show']);
+    Route::put('/main/edit/{id}', [MainTaskController::class, 'edit']);
+    Route::delete('/main/delete/{id}', [MainTaskController::class, 'delete']);
 
 //Sub task controller routes
-Route::post('/sub/create', [SubTaskController::class, 'create']);
-Route::get('/sub/', [SubTaskController::class, 'index']);
-Route::get('/sub/{id}', [SubTaskController::class, 'show']);
-Route::put('/sub/edit/{id}', [SubTaskController::class, 'edit']);
-Route::delete('/sub/delete/{id}', [SubTaskController::class, 'delete']);
+    Route::post('/sub/create', [SubTaskController::class, 'create']);
+    Route::get('/sub/', [SubTaskController::class, 'index']);
+    Route::get('/sub/{id}', [SubTaskController::class, 'show']);
+    Route::put('/sub/edit/{id}', [SubTaskController::class, 'edit']);
+    Route::delete('/sub/delete/{id}', [SubTaskController::class, 'delete']);
+});
