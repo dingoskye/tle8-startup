a7fe1fe0548b84094a373f2f72007d54cdf84b4b
Christa <christa_pol1@hotmail.com>
2026-06-08 15:09:05 +0200

    Some small changes

--- front-end/src/components/sub-task-card.jsx
+++ front-end/src/components/sub-task-card.jsx
@@ -6,21 +6,21 @@ import {LuSquare, LuSquareCheckBig} from "react-icons/lu";
 function SubTaskCard({sub, variant}) {
 
     return (
-        <div className="col-span-1 md:col-span-1">
+        <div className="col-span-1">
             <Card variant={variant}>
                 {!sub.completed ?
                     <div className="flex gap-4 items-center">
                         <p className="sr-only">To do item niet af</p>
-                        <LuSquare className="text-2xl bg-white shadow-sm rounded-md"/>
-                        <p className="text-center font-headers pt-2">{sub.title}</p>
+                        <LuSquare className="text-2xl bg-white shadow-sm rounded-sm"/>
+                        <p className="font-headers pt-2">{sub.title}</p>
                     </div> :
                     <div className="flex gap-4 items-center">
                         <p className="sr-only">To do item niet af</p>
-                        <LuSquareCheckBig className="text-2xl bg-white shadow-sm rounded-md"/>
-                        <p className="text-center font-headers pt-2 line-through">{sub.title}</p>
+                        <LuSquareCheckBig className="text-2xl bg-white shadow-sm rounded-sm"/>
+                        <p className="font-headers pt-2 line-through">{sub.title}</p>
                     </div>
                 }
-                <p>{sub.description ?? "Geen beschrijving"}</p>
+                {sub.description ? <p>{sub.description}</p> : null}
             </Card>
         </div>
     )
