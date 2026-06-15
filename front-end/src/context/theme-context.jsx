e921c15775f0e3686a0b957b2db231da3ebd7f6a
Christa <christa_pol1@hotmail.com>
2026-06-15 12:43:27 +0200

    3 themes added, font change added

--- front-end/src/context/theme-context.jsx
+++ front-end/src/context/theme-context.jsx
@@ -6,10 +6,11 @@ const ThemeContext = createContext()
 export function ThemeProvider({children}) {
     const {apiFetch} = useApi();
     const [theme, setTheme] = useState("theme-default")
+    const [font, setFont] = useState("font-default")
 
     useEffect(() => {
-        document.documentElement.className = theme;
-    }, [theme]);
+        document.documentElement.className = theme + font;
+    }, [theme, font]);
 
     return (
         <ThemeContext.Provider value={{
