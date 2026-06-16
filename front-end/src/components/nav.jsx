68360065edaf533ad0793deab45f6683c7054464
Christa <christa_pol1@hotmail.com>
2026-06-16 14:18:10 +0200

    Small WCAG changes

--- front-end/src/components/nav.jsx
+++ front-end/src/components/nav.jsx
@@ -12,19 +12,29 @@ function Nav() {
         location.pathname === "/" ?
             <nav role="navigation"
                  className="bg-nav-brown min-h-[8vh] max-h-[16vh] mt-6 flex items-center justify-between px-[12vw] gap-2">
-                <NavLinks variant="small" link="/"><FaHome className="text-xl"/></NavLinks>
-                <NavLinks variant="small" link="/hoofdtaken"><FaTasks className="text-xl"/></NavLinks>
-                <NavLinks variant="big" link="/hoofdtaak/aanmaken"><FaPlus className="text-3xl"/></NavLinks>
-                <NavLinks variant="small" link="/"><LuInbox className="text-xl"/></NavLinks>
-                <NavLinks variant="small" link="/profiel"><IoPerson className="text-xl"/></NavLinks>
+                <NavLinks variant="small" link="/"><FaHome className="text-xl"/>
+                    <p className="sr-only">Home</p></NavLinks>
+                <NavLinks variant="small" link="/hoofdtaken"><FaTasks className="text-xl"/>
+                    <p className="sr-only">Hoofdtaken overzicht</p></NavLinks>
+                <NavLinks variant="big" link="/hoofdtaak/aanmaken"><FaPlus className="text-3xl"/>
+                    <p className="sr-only">Groep aanmaken</p></NavLinks>
+                <NavLinks variant="small" link="/"><LuInbox className="text-xl"/>
+                    <p className="sr-only">Groepscode invoeren</p></NavLinks>
+                <NavLinks variant="small" link="/profiel"><IoPerson className="text-xl"/>
+                    <p className="sr-only">Profiel</p></NavLinks>
             </nav> :
             <nav role="navigation"
                  className="bg-nav-brown min-h-[8vh] max-h-[16vh] mt-6 flex items-center justify-between px-[12vw] gap-2">
-                <NavLinks variant="small" link="/hoofdtaak/aanmaken"><FaPlus className="text-xl"/></NavLinks>
-                <NavLinks variant="small" link="/hoofdtaken"><FaTasks className="text-xl"/></NavLinks>
-                <NavLinks variant="big" link="/"><FaHome className="text-3xl"/></NavLinks>
-                <NavLinks variant="small" link="/"><LuInbox className="text-xl"/></NavLinks>
-                <NavLinks variant="small" link="/profiel"><IoPerson className="text-xl"/></NavLinks>
+                <NavLinks variant="small" link="/hoofdtaak/aanmaken"><FaPlus className="text-xl"/>
+                    <p className="sr-only">Groep aanmaken</p></NavLinks>
+                <NavLinks variant="small" link="/hoofdtaken"><FaTasks className="text-xl"/>
+                    <p className="sr-only">Hoofdtaken overzicht</p></NavLinks>
+                <NavLinks variant="big" link="/"><FaHome className="text-3xl"/>
+                    <p className="sr-only">Home</p></NavLinks>
+                <NavLinks variant="small" link="/"><LuInbox className="text-xl"/>
+                    <p className="sr-only">Groepscode invoeren</p></NavLinks>
+                <NavLinks variant="small" link="/profiel"><IoPerson className="text-xl"/>
+                    <p className="sr-only">Profiel</p></NavLinks>
             </nav>
     )
 }
--- front-end/src/components/ui/progressbar.jsx
+++ front-end/src/components/ui/progressbar.jsx
@@ -1,5 +1,3 @@
-import {BsPaperclip} from "react-icons/bs";
-import {FaRegCircle} from "react-icons/fa";
 import {GiPlainCircle} from "react-icons/gi";
 
 function Progressbar({progress}) {
@@ -14,13 +12,13 @@ function Progressbar({progress}) {
     }
 
     return (
-        <div className="relative w-full h-full rounded-full border-white border-4 bg-gray-300 shadow-sm">
+        <div className="relative mx-auto w-[95%] h-full rounded-full border-white border-4 bg-gray-300 shadow-sm">
             <div className="overflow-hidden w-full h-full rounded-full">
                 <div className={`h-full ${color}`} style={{width: `${progress}%`}}/>
             </div>
             <GiPlainCircle
-                className="absolute top-1/2 -translate-y-1/2 -translate-x-3/4 text-4xl bg-white border-white border-3 rounded-full text-nav-brown shadow-sm"
-                style={{left: `${progress}%`}}/>
+                className="absolute top-1/2 -translate-y-1/2 -translate-x-15/16 text-4xl bg-white border-white border-3 rounded-full text-nav-brown shadow-sm"
+                style={{left: `${progress + 5}%`}}/>
             <p className="sr-only">Progressiebalk die laat zien dat je progressie ${progress}% is</p>
         </div>
     )
