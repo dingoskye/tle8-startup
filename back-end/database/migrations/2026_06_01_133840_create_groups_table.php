cac0e58be1fe770afdcc3a48372e6cf478592e47
Christa <christa_pol1@hotmail.com>
2026-06-19 10:36:41 +0200

    Bug with JWT fixes for server and create group bug fixed

--- back-end/database/migrations/2026_06_01_133840_create_groups_table.php
+++ back-end/database/migrations/2026_06_01_133840_create_groups_table.php
@@ -15,7 +15,6 @@ return new class extends Migration {
             $table->string('name');
             $table->text('description')->nullable();
             $table->string('image')->nullable();
-            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
             $table->softDeletes();
         });
